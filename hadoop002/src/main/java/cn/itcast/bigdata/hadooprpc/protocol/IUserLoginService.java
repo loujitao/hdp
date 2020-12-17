c9aae827e6717c9c8754d6e0c33f3902e40efc01
loujitao <995268272@qq.com>
2020-12-17 16:02:43 +0800

    添加注释

--- hadoop002/src/main/java/cn/itcast/bigdata/hadooprpc/protocol/IUserLoginService.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.bigdata.hadooprpc.protocol;
-
-public interface IUserLoginService {
-
-	public static final long versionID = 100L;
-	public String login(String name, String passwd);
-	
-}
--- zookeeperDemo/src/main/java/com/steve/zk/server/DistributeClient.java
+++ zookeeperDemo/src/main/java/com/steve/zk/server/DistributeClient.java
@@ -9,7 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @Description: TODO
+ * @Description: 服务注册中心动态监听原理
+ *  监听servers节点下有无新增；（服务注册或下线）
+ *  有变更重新获取该节点下所有子节点，并获取数据（注册列表）返回
+ *
  * @Author: SteveTao
  * @Date: 2020/9/911:35
  **/
@@ -38,6 +41,7 @@ public class DistributeClient {
     // 获取服务器列表信息
     public void getServerList() throws Exception {
         // 1获取服务器子节点信息，并且对父节点进行监听
+        // 监听器是一次性的；这里默认使用getConnect（）中的监听器，实现循环注册
         List<String> children = zk.getChildren(parentNode, true);
         // 2存储服务器信息列表
         ArrayList<String> servers = new ArrayList<>();
